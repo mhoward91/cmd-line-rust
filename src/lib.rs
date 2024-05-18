d5d58a41cae372c508c6cf57bdb1b3a2a4c01608
Matt Howard <matthew.howard-zambrano@bt.com>
2024-05-18 19:30:07 +0100

    use iterators instead of for loops

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::{fs, env};
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
@@ -8,15 +8,28 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(
+        mut args: impl Iterator<Item = String>  // use of associated types
+    ) -> Result<Self, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path")
+        };
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Self { query, file_path, ignore_case })
+        Ok(Self {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
@@ -27,24 +40,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     } else {
         search(&config.query, &contents)
     };
-    
+
     for line in results {
         println!("{line}");
     }
     Ok(())
 }
 
-pub fn search<'a>(
-    query: &str,
-    contents: &'a str,    
-) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|&line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
@@ -52,13 +59,10 @@ pub fn search_case_insensitive<'a>(
     contents: &'a str
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|&line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
@@ -74,12 +78,9 @@ safe, fast, productive.
 Pick three.
 Duct tape.";
 
-        assert_eq!(
-            vec!["safe, fast, productive."],
-            search(query, contents)
-        );
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
-    
+
     #[test]
     fn case_insensitive() {
         let query = "rUsT";
@@ -88,11 +89,10 @@ Rust:
 safe, fast, productive.
 Pick three.
 Trust me.";
-    
+
         assert_eq!(
             vec!["Rust:", "Trust me."],
             search_case_insensitive(query, contents)
         );
-        
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 // run program with cargo run -- {string to search} poem.txt > output.txt
 // -- means program arguments rather than arguments to cargo
 // > means direct output to txt file instead of terminal
-// eprintln! macro being used to direct these messages to the standard error stream, 
+// eprintln! macro being used to direct these messages to the standard error stream,
 // meaning use of > will only send standard output to the txt file
 // can use environment variable IGNORE_CASE to do a case insensitive search
 // set by calling IGNORE_CASE=1 ahead of the cargo run command
 
-use std::{env, process};
 use minigrep::Config;
+use std::{env, process};
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    
+
     if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
